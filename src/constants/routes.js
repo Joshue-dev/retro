f73c77ec37d7828c06ece16c1b644b019d031a4f
Josh <ukojoshy@gmail.com>
2025-04-10 16:10:31 +0100

    Update routes.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -14,32 +14,8 @@ export const BASE_ROUTE = "https://matadortrust.com";
 
 const isEnvDev = process && process.env.NODE_ENV === "development";
 
-let storeDomain;
+let storeDomain = "app.veritasihomes.com";
 
-if (process.env.NODE_ENV === "development") {
-  //Store Domain for development
-
-  // storeDomain = "orangetestapp.6787878.com";
-  // storeDomain = 'dickson-store.6787878.com';
-  // storeDomain = "ahmedibraheem.6787878.com";
-  storeDomain = "app.veritasihomes.com";
-  // storeDomain = 'jola.6787878.com';
-  // storeDomain = 'mainstone.6787878.com';
-  // storeDomain = 'tolulope.6787878.com';
-  // storeDomain = 'https://app.adozillionhomesng.com'?.split('//')[1];
-  // storeDomain = 'proptech.6787878.com';
-
-  // storeDomain = 'piushomes.6787878.com';
-  // storeDomain = 'joseph_store.6787878.com';
-  // storeDomain = 'ethernit.6787878.com';
-} else {
-  //Store Domain for Production
-
-  storeDomain =
-    typeof window !== "undefined" && window.location.origin && !isEnvDev
-      ? window.location.origin?.split("//")[1]
-      : null;
-}
 export const STORE__DOMAIN = storeDomain;
 
 export const store_name = () =>
